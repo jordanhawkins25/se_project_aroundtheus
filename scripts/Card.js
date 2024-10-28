6ee6c7f32fa19a2fcaceabf133cc4dd4f13fdb79
JordanHawkins <jordanhawkins25@yahoo.com>
2024-10-27 17:12:13 -0700

    trouble with Js

--- scripts/Card.js
+++ scripts/Card.js
@@ -4,7 +4,18 @@ class Card {
           this._name = cardData.name;
           this._link = cardData.link;
 
+          this._likeButton = this
+
           this._cardSelector = cardSelector
+    }
+
+    _getTemplate() {
+        const cardEl = document
+        .querySelector(this._cardSelector)
+        .content.querySelector(".card")
+        .cloneNode(true);
+
+        return cardEl;
     }
 
     _setEventListeners() {
@@ -27,17 +38,17 @@ class Card {
           });
     }
 
-    _getTemplate() {
-        return document.
-        querySelector(this._cardSelector)
-        .content.querySelector(".card")
-        .cloneNode(true);
-    }
 
     cardView() {
        this._element = this._getTemplate();
-       
+      
        this._setEventListeners();
+
+       this._element.querySelector(
+        ".imageEl"
+       ).style =   `url(${this._link}`;
+        this._element.querySelector(".card__title").textContent = this._text;
+       
     }
 
 }
--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,55 +1,62 @@
 class FormValidator {
 
-   constructor(settings, formEl) {
-    this._inputSelector = settings.inputSelector
-    this._submitButtonSelector = settings.submitButtonSelector
-    this._inactiveButtonClass = settings.inactiveButtonClass
-    this._inputErrorClass = settings.inputErrorClass
-    this._errorClass = settings.errorClass
+  constructor(settings, formEl) {
+   this._inputSelector = settings.inputSelector
+   this._submitButtonSelector = settings.submitButtonSelector
+   this._inactiveButtonClass = settings.inactiveButtonClass
+   this._inputErrorClass = settings.inputErrorClass
+   this._errorClass = settings.errorClass
 
-    this._form = formEl;
- }
+   this._form = formEl;
+}
 
- _showInputError(inputEl, errorMessage) {
-    const errorMessageEl = this._formEl.querySelector(`#${inputEl.id}-error`);
-  inputEl.classList.remove(this._inputErrorClass);
-  errorMessageEl.textContent = "";
-  errorMessageEl.classList.remove(this._errorClass);
- }
+_showInputError(inputEl, errorMessage) {
+   const errorMessageEl = this.formEl.querySelector(`#${inputEl.id}-error`);
+ inputEl.classList.remove(this._inputErrorClass);
+ errorMessageEl.textContent = "";
+ errorMessageEl.classList.remove(this._errorClass);
+}
 
- toggleButtonState() {
-   
+toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
+  if (hasInvalidInput(inputEls)) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+    return;
   }
-
-  hasInvalidInput() {
-
-  }
-
-  _checkInputValidity() {
-
-  }
-
- _setEventListeners() {
-    this._inputEls = [this.querySelectorAll(this._inputSelector)];
-    this._submitButton = this.querySelector(this._submitButtonSelector);
- toggleButtonState(inputEls, submitButton, options);
- inputEls.forEach((inputEl) => {
-   inputEl.addEventListener("input", () => {
-     checkInputValidity(this, inputEl, options);
-     toggleButtonState(inputEl, submitButton, options);
-   });
- });
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
  }
 
- enableValidation() {
-    this._form.addEventListener("siubmit", (e) => {
-        e.preventDefault();
-      });
-      setEventListeners(formEl, options);
+ hasInvalidInput(inputList) {
+  return !inputList.every((inputEl) => inputEl.validity.valid);
  }
+
+ _checkInputValidity() {
+  if (!_inputEl.validity.valid) {
+    return showInputError(_inputEl, options);
+  }
+  hideInputError(inputEl, options);
+ }
+
+_setEventListeners() {
+   this._inputEls = [this.querySelectorAll(this._inputSelector)];
+   this._submitButton = this.querySelector(this._submitButtonSelector);
+toggleButtonState(inputEls, submitButton, options);
+inputEls.forEach((inputEl) => {
+  inputEl.addEventListener("input", () => {
+    checkInputValidity(this, inputEl, options);
+    toggleButtonState(inputEl, submitButton, options);
+  });
+});
+}
+
+enableValidation() {
+   this._form.addEventListener("siubmit", (e) => {
+       e.preventDefault();
+     });
+     setEventListeners(formEl, options);
+}
 
 }
 
 export default FormValidator;
- 
-
